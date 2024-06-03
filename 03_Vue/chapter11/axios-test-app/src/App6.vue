1256eb3d88fced4c7c388ecfa5319d5af9d31b77
gener-lst <ofin0613@naver.com>
2024-06-03 17:45:04 +0900

    0603_session

---
+++ 03_Vue/chapter11/axios-test-app/src/App6.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+// await를 쓰기 위해서는 함수 앞에 async를 붙여줌
+const requestAPI = async () => {
+  const url = '/api/todos';
+  try {
+    const response = await axios.get(url, { timeout: 900 });
+    console.log('# 응답객체 : ', response);
+  } catch (e) {
+    console.log('## 다음 오류가 발생했습니다.');
+    // instanceof : e가 Error 타입인지 체크
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  }
+};
+
+requestAPI();
+</script>
